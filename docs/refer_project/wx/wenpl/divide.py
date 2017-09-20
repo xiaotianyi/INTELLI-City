65954b5fbc66b0e97032bc3b624aa8aba9646674
小天 <yimifoliage@gmail.com>
2017-09-20 11:01:23 +0800

    no message

--- docs/refer_project/wx/wenpl/divide.py
+++ docs/refer_project/wx/wenpl/divide.py
@@ -221,12 +221,13 @@ def getResult(url):
     if url[:4] != "http":
         url = 'http://www.intellense.com:3080' + url
         web_flag = 1
+    elif url == "http://60.190.248.2:9990/admin":
+        return "登录请打开网址" + url
     print "url", url
 
     req = urllib2.Request(url)
     req.add_header('authorization', token) if web_flag else None
     response = urllib2.urlopen(req)
-    print "response", response.read()
 
     #     req = urllib2.Request(url)
     #     print "req", req
