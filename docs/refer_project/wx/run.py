195401d54a2a30394bed82d92cfc16a9bed770e1
小天 <yimifoliage@gmail.com>
2017-10-11 17:21:05 +0800

    no message

---
+++ docs/refer_project/wx/run.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# filename: main.py
+import web
+# from handle import Handle
+
+urls = (
+    '/wx', 'Handle',
+)
+
+app = web.application(urls, globals())
+
+if __name__ == '__main__':
+    app.run()
