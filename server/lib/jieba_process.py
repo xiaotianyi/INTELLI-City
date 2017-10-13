fc8828887cb72ee6c5cd1cabc07a2b04225e58de
小天 <yimifoliage@gmail.com>
2017-10-13 10:16:38 +0800

    no message

---
+++ server/lib/jieba_process.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import json
+import time
+
+
+import jieba
+import jieba.posseg as pseg
+
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+cwd = os.getcwd()
+
+# main called function
+def prepare_dicts():
+    # people = getPeople()
+    # cities = getPosition('cities')
+    # towns = getPosition('towns')
+    # stations = getPosition('stations')
+    # devices = getPositpassion('devices')
+    # positions = merge_positions([cities, towns, stations, devices])
+    # points = getPoints()
+    # pro = getPros()
+    # general = getGenerals()
+    # paraCategory = dict(positions, **people)
+    # dict1 = dict(general, **pro)
+    # dict2 = dict(dict1, **paraCategory)
+    # st = getStore()  # store dict
+    pass
+
+
+def get_people():
+    pass
