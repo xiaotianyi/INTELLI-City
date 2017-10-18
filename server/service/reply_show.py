73a361c57e4e54adb1e3dd1cafadde59a516a8ba
小天 <yimifoliage@gmail.com>
2017-10-18 20:51:06 +0800

    no message

--- server/service/reply_show.py
+++ server/service/reply_show.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from service_call import connect_turing, connect_rest_api
+
 
 def show_reply():
-    pass
+    response, flag = connect_rest_api()
+    reply = show_diff_result(response, flag)
+    if reply:
+        return
+    else:
+        return
+
+
+def show_diff_result(response, flag):
+    return response
