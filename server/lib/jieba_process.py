12d26f77fa9b961835d06f4f31b6338e5bfae23f
小天 <yimifoliage@gmail.com>
2017-10-18 20:50:52 +0800

    no message

--- server/lib/jieba_process.py
+++ server/lib/jieba_process.py
@@ -19,32 +19,19 @@ dict_url = cwd + '/lib/data_base/'
 
 # main called function
 def prepare_dicts():
-    # people = getPeople()
-    # cities = getPosition('cities')
-    # towns = getPosition('towns')
-    # stations = getPosition('stations')
-    # devices = getPositpassion('devices')
-
     # TODO 需要对cities, towns, stations, devices进行转化
     # positions = merge_positions([cities, towns, stations, devices])
-
-    # points = getPoints()
-    # pro = getPros()
-    # general = getGenerals()
-    # paraCategory = dict(positions, **people)
-    # dict1 = dict(general, **pro)
-    # dict2 = dict(dict1, **paraCategory)
-    # st = getStore()  # store dict
 
     # 加载各支持字典
     platform_dict = get_dict("intellence.json")
     pro_dict = get_dict("pro.json")
     general_dict = get_dict("general.json")
-    words_dict = dict(pro_dict, general_dict)
+    words_dict = dict(pro_dict, **general_dict)
     all_dicts = dict(words_dict, **platform_dict)
 
     # 加载api字典
     api_dict = get_dict("api.json")
+    return
 
 
 # 各路存储的字典
